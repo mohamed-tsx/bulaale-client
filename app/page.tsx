66b137282c49ae1870318413f25504932620381d
Mohamed Mohamud <moamiin79@gmail.com>
2025-10-14 15:14:52 +0300

    Refactor Home page to integrate ProductsPage component, replacing multiple individual components for a streamlined layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,9 @@
-import { Header } from "@/components/Home/header"
-import { Hero } from "@/components/Home/hero"
-import { CategoryCircles } from "@/components/Home/category-circles"
-import { CategoryGrid } from "@/components/Home/category-grid"
-import { FeaturedProducts } from "@/components/Home/featured-products"
-import { WhyChooseUs } from "@/components/Home/why-choose-us"
-import { Testimonials } from "@/components/Home/testimonials"
-import { Newsletter } from "@/components/Home/newsletter"
-import { DiscountBanner } from "@/components/DiscountBanner"
+import ProductsPage from "./products/page";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Hero />
-      <DiscountBanner />
-      <CategoryCircles />
-      <CategoryGrid />
-      <FeaturedProducts />
-      <WhyChooseUs />
-      <Testimonials />
-      <Newsletter />
+      <ProductsPage />
     </main>
   )
 }
